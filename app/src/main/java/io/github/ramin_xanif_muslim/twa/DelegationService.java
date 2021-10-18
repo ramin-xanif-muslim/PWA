c909bfb18c40baa6196cd2bddad0f571c00e8e49
Ramin <raminxanifmuslim@gmail.com>
2021-10-18 17:43:44 +0400

    Bubblewrap

---
+++ app/src/main/java/io/github/ramin_xanif_muslim/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package io.github.ramin_xanif_muslim.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
