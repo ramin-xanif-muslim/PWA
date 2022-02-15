a63ec3d039cf793fb99aafd07bd98cb45cbfc74b
Ali <Xliy_g8zy@itstep.edu.az>
2022-02-15 11:27:25 +0400

    changed sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -7,91 +7,89 @@ import "../styles/SideBar.css";
 import { useGlobalContext } from "../config/context";
 
 const Sidebar = () => {
-	const [isChecked, setIsChecked] = useState(false);
-    const { logout } = useGlobalContext()
+    const [isChecked, setIsChecked] = useState(false);
+    const { logout } = useGlobalContext();
 
     const onLogout = () => {
-		localStorage.clear();
-        logout()
-    }
+        localStorage.clear();
+        logout();
+    };
 
-	return (
-		<aside className="sidebar" >
-			<div className="sidebar-header">
-				<div className="sidebar-header-top">
-					<img src={ProfilSidebarHeader} />
-					<p>BALANS</p>
-					<img src={chat} />
-				</div>
-				<div className="sidebar-header-top">
-					<p>Euroclima</p>
-					<p>euroclima@admin</p>
-				</div>
-			</div>
-			<ul className="links">
-				<div className="sidebar_title">Profil ayarları</div>
-				{sidebarProfileLinks.map((item) => {
-					const { id, url, text, icon, icon_h } = item;
+    return (
+        <aside className="sidebar">
+            <div className="sidebar-header">
+                <div className="sidebar-header-top">
+                    <img src={ProfilSidebarHeader} />
+                    <p>BALANS</p>
+                    <img src={chat} />
+                </div>
+                <div className="sidebar-header-top">
+                    <p>Euroclima</p>
+                    <p>euroclima@admin</p>
+                </div>
+            </div>
+            <ul className="links">
+                <div className="sidebar_title">Profil ayarları</div>
+                {sidebarProfileLinks.map((item) => {
+                    const { id, url, text, icon, icon_h } = item;
 
-					const onClick = () => {
-						setIsChecked(id);
-					};
-					return (
-						<li key={id} onClick={onClick}>
-							<Link to={url}>
-								<img src={isChecked === id ? icon_h : icon} />
-								<div
-									className={
-										isChecked === id
-											? "links_text_h"
-											: "links_text"
-									}
-								>
-									{text}
-								</div>
-							</Link>
-						</li>
-					);
-				})}
-				<div className="sidebar_title">Ayarlar</div>
-				{sidebarSettingsLinks.map((item) => {
-					const { id, url, text, icon, icon_h } = item;
+                    const onClick = () => {
+                        setIsChecked(id);
+                    };
+                    return (
+                        <li key={id} onClick={onClick}>
+                            <Link to={url}>
+                                <img src={isChecked === id ? icon_h : icon} />
+                                <div
+                                    className={
+                                        isChecked === id
+                                            ? "links_text_h"
+                                            : "links_text"
+                                    }
+                                >
+                                    {text}
+                                </div>
+                            </Link>
+                        </li>
+                    );
+                })}
+                <div className="sidebar_title">Ayarlar</div>
+                {sidebarSettingsLinks.map((item) => {
+                    const { id, url, text, icon, icon_h } = item;
 
-					const onClick = () => {
-						setIsChecked(id);
-					};
-					return (
-						<li key={id} onClick={onClick}>
-							<Link to={url}>
-								<img src={isChecked === id ? icon_h : icon} />
-								<div
-									className={
-										isChecked === id
-											? "links_text_h"
-											: "links_text"
-									}
-								>
-									{text}
-								</div>
-							</Link>
-						</li>
-					);
-				})}
-						<li onClick={onLogout}>
-								{/* <img src={isChecked === 10 ? icon_h : icon} /> */}
-								<div
-									className={
-										isChecked === 10
-											? "links_text_h"
-											: "links_text"
-									}
-								>
-									Çıxış
-								</div>
-						</li>
-			</ul>
-		</aside>
-	);
+                    const onClick = () => {
+                        setIsChecked(id);
+                    };
+                    return (
+                        <li key={id} onClick={onClick}>
+                            <Link to={url}>
+                                <img src={isChecked === id ? icon_h : icon} />
+                                <div
+                                    className={
+                                        isChecked === id
+                                            ? "links_text_h"
+                                            : "links_text"
+                                    }
+                                >
+                                    {text}
+                                </div>
+                            </Link>
+                        </li>
+                    );
+                })}
+                <li onClick={onLogout}>
+                    {/* <img src={isChecked === 10 ? icon_h : icon} /> */}
+                    <div
+                        className={
+                            isChecked === 10 ? "links_text_h" : "links_text"
+                        }
+                    >
+                        Çıxış
+                    </div>
+                </li>
+            </ul>
+        </aside>
+    );
 };
 
 export default Sidebar;
